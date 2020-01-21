6c542dd6dda705ea6b4a426e891e7260b30f11dd
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 20:45:44 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,19 @@
-print("hello")
-print("world")
-print("hello")
+def merge(L1: list, L2: list) ->list:
+    """ Merge sorted lists L1 and L2 into a new list and return that new list.
+    merge([1,2,3,4,5,6,7])
+    """
+    
+    newL = []
+    i1 = 0
+    i2 = 0
+    #For each pair of items L1[i1] and L2[i2], copy the smaller into newL.
+    while i1 != len(L1) and i2 != len(L2):
+       if L1[i] <= L2[i2]:
+          newL.append(L1[i1])
+          i1 += 1
+       else:
+        newL.append(L2[i2])
+        i2 +=1
+    newL.extend(L1[i1:])
+    newL.extend(L2[i2:])
+    return newL
